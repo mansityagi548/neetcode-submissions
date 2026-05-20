83610442012e37d74c5d0f7bc9acd17180f9780f
mansityagi548 <mansityagi10c@gmail.com>
2026-05-20 08:39:15 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+  std::sort(nums.begin() , nums.end());
+  int n = nums.size();
+  int candidate = 0;
+  int count = 0;
+  for(int i = 0 ; i < nums.size() ; i++){
+     if(nums[i] == nums[candidate]){
+      count++;
+     }else{
+       if(count > (n/2)) return nums[candidate];
+       else{
+         count = 0;
+         candidate = i;
+         i--;
+       }
+     }
+  }
+  
+
+  return nums[candidate];
+      
+    }
+};
