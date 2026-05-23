28893d452920a0df6e97049c6cf54ed6d6bb3e09
mansityagi548 <mansityagi10c@gmail.com>
2026-05-23 08:47:43 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    void sortColors(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = 0;
+
+        while (mid <= end) {
+            if (nums[mid] == 0) {
+                std::swap(nums[start], nums[mid]);
+                start++, mid++;
+            } else if (nums[mid] == 2) {
+                std::swap(nums[end], nums[mid]);
+                end--;
+            } else if (nums[mid] == 1)
+                mid++;
+        }
+    }
+};
